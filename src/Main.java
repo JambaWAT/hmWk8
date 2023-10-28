a166dbe175e2cb9a2705a8b7b8b636d933422235
JambaWAT <Puskinalex38@gmail.com>
2023-10-28 16:26:24 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,33 @@ public class Main {
         {
             int[] colors = new int[]{1, 2, 3};
             float[] stars = new float[]{1.57f, 7.654f, 9.986f};
-            boolean[] logicMoves = new boolean[12];
-            System.out.println("Массив 1 в неправильном порядке: " + Arrays.toString(colors));
+            int[] arr3 = new int[4];
+            arr3[0] = 1;
+            arr3[1] = 5;
+            arr3[2]= 7;
+            arr3[3] = 6;
+            System.out.println("Массив 1 в правильном порядке: " + Arrays.toString(colors));
             System.out.println("Массив 2 в правильном порядке: " + Arrays.toString(stars));
-            System.out.println("Массив 3 в правильном порядке: " + Arrays.toString(logicMoves));
+            System.out.println("Массив 3 в правильном порядке: " + Arrays.toString(arr3));
             System.out.println("Массив 1 в неправильном порядке: ");
             for (int i = colors.length - 1; i >= 0; i--) {
-                System.out.print(colors[i] + ", ");
+                {if(colors[i] == colors[0])
+                    System.out.print(colors[i] + " ");
+                else System.out.print(colors[i] + ", ");}
             }
             System.out.println();
             System.out.println("Массив 2 в неправильном порядке: ");
             for (int i = stars.length - 1; i >= 0; i--) {
-                System.out.print(stars[i] + " ");
+                {if(stars[i] == stars[0])
+                    System.out.print(stars[i] + " ");
+                else System.out.print(stars[i] + ", ");}
             }
             System.out.println();
             System.out.println("Массив 3 в неправильном порядке:  ");
-            for (int i = logicMoves.length - 1; i >= 0; i--) {
-                System.out.print(logicMoves[i] + " ");
+            for (int i = arr3.length - 1; i >= 0; i--) {
+                {{if(arr3[i] == arr3[0])
+                    System.out.print(arr3[i] + " ");
+                else System.out.print(arr3[i] + ", ");}}
 
             }
             for(int i = 0; i < colors.length; i++) {
